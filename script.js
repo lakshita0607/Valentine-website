8f74e91630b82cade7da5327f495980068c917d6
lakshita0607 <lakshita0307@gmail.com>
2025-01-30 11:03:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function for the "Yes" button
+function onYesClick() {
+    alert("Yay Lakshita is your forever valentine");
+}
+
+// Function for the "No" button
+function onNoClick() {
+    alert("Sorry, you've selected a wrong option. Try again.");
+}
